591ba8bb520f7ac2f3d9fc2cecc06d57e9f62af2
Camilo.Victoria@newtonvisionco.com <Camilo.Victoria@newtonvisionco.com>
2024-08-27 14:07:54 -0500

    feat: vercel config

--- index.js
+++ index.js
@@ -1,123 +1,120 @@
-import express from "express";
-import path from "path";
-import fs from "fs/promises";
-import cors from "cors";
-import crypto from "crypto"; // Added to generate UUIDs
+import express from 'express'
+import path from 'path'
+import fs from 'fs/promises'
+import cors from 'cors'
+import crypto from 'crypto' //* Added to generate UUIDs
+import dataArt from './art.json' assert {type: 'json'}
 // *Schemas
 import {
   validateCreateArt,
-  validatePartialArt,
-} from "./schemas/artSchemas/artSchemas.js";
+  validatePartialArt
+} from './schemas/artSchemas/artSchemas.js'
 
-const PORT = process.env.PORT ?? 8080;
+const PORT = process.env.PORT ?? 8080
 
-const app = express();
+const app = express()
 
-app.disable("x-powered-by");
+app.disable('x-powered-by')
 
 //* Middleware to parse JSON bodies
-app.use(express.json());
+app.use(express.json())
 
 app.use(
   cors({
     origin: (origin, callback) => {
-      const ACCEPTED_ORIGINS = ["http://localhost:8080"];
+      const ACCEPTED_ORIGINS = ['http://localhost:8080']
 
       if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
-        return callback(null, true);
+        return callback(null, true)
       }
 
-      return callback(new Error("Not allowed by CORS"));
-    },
+      return callback(new Error('Not allowed by CORS'))
+    }
   })
-);
+)
 
 // Read JSON file asynchronously
-const jsonFilePath = path.resolve("./art.json");
+const jsonFilePath = path.resolve('./art.json')
 
-const getDataArt = async () => {
-  //* Read and parse the JSON file
-  const data = await fs.readFile(jsonFilePath, "utf-8");
-  return JSON.parse(data);
-};
+// const getDataArt = async () => {
+//   //* Read and parse the JSON file
+//   const data = await fs.readFile(jsonFilePath, 'utf-8')
+//   return JSON.parse(data)
+// }
 
 //* Endpoint to get art data
-app.get("/paints", async (req, res) => {
-  const dataArt = await getDataArt();
-  const { medium, genre } = req.query;
+app.get('/paints', async (req, res) => {
+  const { medium, genre } = req.query
 
   if (genre) {
     const filteredArts = dataArt.filter((art) =>
       art.genre.some((a) => a.toLowerCase() === genre.toLowerCase())
-    );
-    return res.json(filteredArts);
+    )
+    return res.json(filteredArts)
   }
 
   if (medium) {
     const filteredArts = dataArt.filter(
       (data) => data.medium.toLowerCase() === medium.toLowerCase()
-    );
-    return res.json(filteredArts);
+    )
+    return res.json(filteredArts)
   }
 
-  return res.json(dataArt);
-});
+  return res.json(dataArt)
+})
 
-app.get("/paints/:id", async (req, res) => {
-  const dataArt = await getDataArt();
-  const { id } = req.params;
+app.get('/paints/:id', async (req, res) => {
+  const { id } = req.params
 
-  const art = dataArt.find((art) => art.id === id);
+  const art = dataArt.find((art) => art.id === id)
 
-  if (art) return res.json(art);
+  if (art) return res.json(art)
 
-  res.status(404).json({ message: "Art not Found" });
-});
+  res.status(404).json({ message: 'Art not Found' })
+})
 
 //* Endpoint to update existing art data
-app.patch("/paints/:id", async (req, res) => {
-  const dataArt = await getDataArt();
-  const { id } = req.params;
-  const result = validatePartialArt(req.body);
+app.patch('/paints/:id', async (req, res) => {
+  const { id } = req.params
+  const result = validatePartialArt(req.body)
 
   if (!result.success) {
-    return res.status(400).json({ error: JSON.parse(result.error.message) });
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
   }
 
-  const artIndex = dataArt.findIndex((art) => art.id === id);
+  const artIndex = dataArt.findIndex((art) => art.id === id)
 
   if (artIndex === -1) {
-    return res.status(400).json({ message: "Art not found" });
+    return res.status(400).json({ message: 'Art not found' })
   }
 
   const updateArt = {
     ...dataArt[artIndex],
-    ...result.data,
-  };
+    ...result.data
+  }
 
-  dataArt[artIndex] = updateArt;
+  dataArt[artIndex] = updateArt
 
-  res.status(200).json(updateArt);
-});
+  res.status(200).json(updateArt)
+})
 
 //* Endpoint to create new art data
-app.post("/paints", async (req, res) => {
-  const dataArt = await getDataArt();
-  const result = validateCreateArt(req.body);
+app.post('/paints', async (req, res) => {
+  const result = validateCreateArt(req.body)
 
   if (!result.success) {
-    return res.status(400).json({ error: JSON.parse(result.error.message) });
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
   }
 
   const newArt = {
     id: crypto.randomUUID(),
-    ...result.data,
-  };
+    ...result.data
+  }
 
-  dataArt.push(newArt);
+  dataArt.push(newArt)
 
-  res.status(201).json(newArt);
-});
+  res.status(201).json(newArt)
+})
 
 // // *Cors Origin
 // app.options("/art/:id", (req, res) => {
@@ -138,15 +135,18 @@ app.post("/paints", async (req, res) => {
 // });
 
 //* Root endpoint
-app.get("/", (req, res) => {
-  res.json({ message: "hello world" });
-});
+app.get('/', (req, res) => {
+  res.json({ message: 'hello world' })
+})
 
 //* 404 handler
 app.use((req, res) => {
-  res.status(404).send("<h1>Page Not Found</h1>");
-});
+  res.status(404).send('<h1>Page Not Found</h1>')
+})
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port http://localhost:${PORT}`);
-});
+  console.log(`Server listening on port http://localhost:${PORT}`)
+})
+
+
+export default app;
--- schemas/artSchemas/artSchemas.js
+++ schemas/artSchemas/artSchemas.js
@@ -1,14 +1,14 @@
-import { z } from "zod";
+import { z } from 'zod'
 
 const createArtSchema = z.object({
   title: z.string({
-    invalid_type_error: "title must be a string",
-    required_error: "title is a mandatory field",
+    invalid_type_error: 'title must be a string',
+    required_error: 'title is a mandatory field'
   }),
   artist: z.string(),
   year: z
     .number({
-      required_error: "Year is a mandatory field",
+      required_error: 'Year is a mandatory field'
     })
     .int()
     .min(1500)
@@ -20,31 +20,31 @@ const createArtSchema = z.object({
   price: z.number().positive(),
   genre: z.array(
     z.enum([
-      "Renaissance",
-      "Classical",
-      "Romanticism",
-      "Historical",
-      "Early Netherlandish",
-      "Triptych",
-      "Baroque",
-      "Group Portrait",
-      "Regionalism",
-      "Portrait",
-      "Symbolism",
-      "Romantic",
-      "Mythological",
-      "Renaissance",
-      "Religious",
-      "Cubism",
-      "Political",
+      'Renaissance',
+      'Classical',
+      'Romanticism',
+      'Historical',
+      'Early Netherlandish',
+      'Triptych',
+      'Baroque',
+      'Group Portrait',
+      'Regionalism',
+      'Portrait',
+      'Symbolism',
+      'Romantic',
+      'Mythological',
+      'Renaissance',
+      'Religious',
+      'Cubism',
+      'Political'
     ])
-  ),
-});
+  )
+})
 
-export function validateCreateArt(object) {
-  return createArtSchema.safeParse(object);
+export function validateCreateArt (object) {
+  return createArtSchema.safeParse(object)
 }
 
-export function validatePartialArt(object) {
-  return createArtSchema.partial().safeParse(object);
+export function validatePartialArt (object) {
+  return createArtSchema.partial().safeParse(object)
 }
